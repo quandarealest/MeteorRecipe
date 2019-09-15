435141be42dfc50822400fb9b6cd1a9d656018bf
HiImKwon <buikhacminhquan@hotmail.com>
2019-09-15 22:53:07 +0700

    Init project

---
+++ collections/Recipes.js
@@ -0,0 +1,34 @@
+Recipes = new Mongo.Collection('recipes');
+
+RecipeSchema = new SimpleSchema({
+  name: {
+    type: String,
+    label: "Name"
+  },
+  desc: {
+    type: String,
+    label: "Description"
+  },
+  author: {
+    type: String,
+    label: "Author",
+    autoValue: function(){
+      return this.userId
+    },
+    autoform: {
+      type: "hidden"
+    }
+  },
+  createdAt: {
+    type: Date,
+    label: "Created At",
+    autoValue: function() {
+      return new Date()
+    },
+    autoform: {
+      type: "hidden"
+    }
+  }
+});
+
+Recipes.attachSchema( RecipeSchema );
